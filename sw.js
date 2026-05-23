bcda33fc1389fa0f785eeb16248c97b4dc63fd13
tk-marine-consultancy <tk.marine.consultancy@gmail.com>
2026-05-24 03:58:41 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'pilot-nav-v4';
+const ASSETS = ['./index.html', './manifest.json', './sw.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  // Delete ALL old caches on activation
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
